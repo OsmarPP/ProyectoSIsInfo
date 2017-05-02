7ebe661991ee2430869ca422845879ff407992fc
Psicosis <rob_todrcl_01@hotmail.com>
2017-05-02 12:32:18 -0400

    cambieEmpleado

--- ProyectoPapeletaPago/ProyectoPapeletaPago/Form1.Designer.cs
+++ ProyectoPapeletaPago/ProyectoPapeletaPago/Form1.Designer.cs
@@ -35,9 +35,9 @@
             // 
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(186, 31);
+            this.button1.Size = new System.Drawing.Size(186, 47);
             this.button1.TabIndex = 0;
-            this.button1.Text = "REGISTRAR NUEVO EMPLEADO";
+            this.button1.Text = "REGISTRAR NUEVO EMPLEADOS";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
